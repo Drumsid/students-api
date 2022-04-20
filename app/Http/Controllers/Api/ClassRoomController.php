988691de5cd16a6a6756bd2a000cbf9216b3fdc8
denis.soloduhin <denis.soloduhin@simbirsoft.com>
2022-04-20 18:29:06 +0300

    add classes route

--- app/Http/Controllers/Api/ClassRoomController.php
+++ app/Http/Controllers/Api/ClassRoomController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\ClassRoom;
 use Illuminate\Http\Request;
 
 class ClassRoomController extends Controller
 {
-    //
+    public function index()
+    {
+        return ClassRoom::all();
+    }
 }
