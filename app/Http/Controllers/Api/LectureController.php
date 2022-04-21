d8812d4e15eb588089050087db1f7176a9234c5d
denis.soloduhin <denis.soloduhin@simbirsoft.com>
2022-04-21 10:55:43 +0300

    create lecture request

--- app/Http/Controllers/Api/LectureController.php
+++ app/Http/Controllers/Api/LectureController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LectureRequest;
 use App\Http\Resources\LectureResource;
 use App\Models\Lecture;
 use Illuminate\Http\Request;
@@ -16,9 +17,9 @@ class LectureController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(LectureRequest $request)
     {
-        $lecture = Lecture::create($request->all());
+        $lecture = Lecture::create($request->validated());
         return new LectureResource($lecture);
     }
 
@@ -37,7 +38,7 @@ class LectureController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
     }
 
     /**
---
+++ app/Http/Requests/LectureRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LectureRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "topic" => "required|min:3|max:255|unique:lectures,topic",
+            "description" => "required|min:3|max:255"
+        ];
+    }
+}
